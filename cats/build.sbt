08cd71189e52f37ae00a76e6360b311899227981
once-ler <htao@eztier.com>
2019-09-07 16:31:42 -0400

    Start concreate implementation using cats

--- cats/build.sbt
+++ cats/build.sbt
@@ -9,3 +9,32 @@ resolvers += Resolver.bintrayIvyRepo("com.eed3si9n", "sbt-plugins")
 libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion % "provided"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
+// https://mvnrepository.com/artifact/org.typelevel/cats-core
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.6.0"
+
+lazy val assemblySettings = Seq(
+  assemblyJarName in assembly := s"${name.value}-${version.value}.jar",
+  
+  assemblyMergeStrategy in assembly := {
+    case PathList("org","aopalliance", xs @ _*) => MergeStrategy.last
+    case PathList("javax", "inject", xs @ _*) => MergeStrategy.last
+    case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+    case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+    case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+    case PathList("com", "google", xs @ _*) => MergeStrategy.last
+    case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+    case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+    case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+    case "about.html" => MergeStrategy.rename
+    // case "META-INF/services/org.apache.spark.sql.sources.DataSourceRegister" => MergeStrategy.concat
+    case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+    case "META-INF/mailcap" => MergeStrategy.last
+    case "META-INF/mimetypes.default" => MergeStrategy.last
+    case "plugin.properties" => MergeStrategy.last
+    case "log4j.properties" => MergeStrategy.last
+    case x =>
+      val oldStrategy = (assemblyMergeStrategy in assembly).value
+      oldStrategy(x)
+  },
+  test in assembly := {}
+)
--- cats/src/main/scala/SimpleCats.scala
+++ cats/src/main/scala/SimpleCats.scala
@@ -1,5 +1,228 @@
 package com.eztier.examples
 
+// ./Server.scala
+object Server extends IOApp {
+  def createServer[F[_]]: Resource[F, H4Server[F]] = ???
+  def run(args : List[String]) : IO[ExitCode] = createServer.use(_ => IO.never).as(ExitCode.Success)
+}
+
+// ./Domain
+
+// ./Domain/ValidationError.scala
+
+sealed trait ValidationError extends Product with Serializable
+case object PublicationNotFoundError extends ValidationError
+case object AuthorNotFoundError extends ValidationError
+case class AuthorAlreadyExistsError(author: Author) extends ValidationError
+
+// ./Domain/authors
+
+// ./Domain/authors/Author.scala
+case class Author(
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  id: Option[Long] = None
+)
+
+// ./Domain/authors/AuthorRepositoryAlgebra.scala
+import cats.data.OptionT
+
+trait AuthorRepositoryAlgebra[F[_]] {
+  def get(id: Long): OptionT[F, Author]
+
+  def findByEmail(email: String): OptionT[F, Author]
+}
+
+// ./Domain/authors/AuthorValidationAlgebra.scala
+import cats.data.EitherT
+
+trait AuthorValidationAlgebra[F[_]] {
+  /* Fails with a AuthorAlreadyExistsError */
+  def doesNotExist(author: Author): EitherT[F, AuthorAlreadyExistsError, Unit]
+
+  /* Fails with a AuthorNotFoundError if the authot id does not exist or if it is none */
+  def exists(authorId: Option[Long]): EitherT[F, AuthorNotFoundError.type, Unit]
+}
+
+// ./Domain/authors/AuthorValidationInterpreter.scala
+import cats.Applicative
+import cats.data.EitherT
+import cats.implicits._
+
+class AuthorValidationInterpreter[F[_]: Applicative](authorRepo: AuthorRepositoryAlgebra[F]) extends AuthorValidationAlgebra[F] {
+  def doesNotExist(author: Author): EitherT[F, AuthorAlreadyExistsError, Unit] =
+    authorRepo
+      .findByEmail(author.email)
+      .map(AuthorAlreadyExistsError)
+      .toLeft(())
+
+  def exists(authorId: Option[Long]): EitherT[F, AuthorNotFoundError.type, Unit] =
+    authorId match {
+      case Some(id) =>
+        authorRepo.get(id)
+          .toRight(AuthorNotFoundError)
+          .void
+      case None =>
+        // EitherT.leftT, EitherT.rightT is alias for EitherT.pure
+        EitherT.left[Unit](AuthorNotFoundError.pure[F])
+    }
+}
+
+object AuthorValidationInterpreter {
+  def apply[F[_]: Applicative](repo: AuthorRepositoryAlgebra[F]): AuthorValidationAlgebra[F] =
+    new AuthorValidationInterpreter[F](repo)
+}
+
+// ./Domain/authors/AuthorService.scala
+import cats.data._
+import cats.Functor
+import cats.Monad
+import cats.syntax.functor._
+
+class AuthorService[F[_]](authorRepo: AuthorRepositoryAlgebra[F], validation: AuthorValidationAlgebra[F]) {
+  def getAuthor(id: Long)(implicit F: Functor[F]): EitherT[F, AuthorNotFoundError.type, User] =
+    authorRepo.get(id).toRight(AuthorNotFoundError)
+
+  def getAuthorByEmail(email: String)(implicit F: Functor[F]): EitherT[F, AuthorNotFoundError.type, Author] =
+    authorRepo.findByEmail(email).toRight(AuthorNotFoundError)
+}
+
+object AuthorService {
+  def apply[F[_]](repository: AuthorRepositoryAlgebra[F], validation: AuthorValidationAlgebra[F]): AuthorService[F] =
+    new AuthorService[F](repository, validation)
+}
+
+
+// ./Domain/publications
+
+// ./Domain/publications/Publication.scala
+import java.time.Instant
+
+case class Publication(
+  publicationDate: Option[Instant] = None,
+  title: String,
+  id: Option[Long] = None,
+  authorId: Long
+)
+
+// ./Domain/publications/PublicationRepositoryAlgebra.scala
+trait PublicationRepositoryAlgebra[F[_]] {
+  def get(id: Long): F[Option[Publication]]
+
+  def findByAuthorId(authorId: Long): F[List[Publication]]
+}
+
+// ./Domain/publications/PublicationValidationAlgebra.scala
+import cats.data.EitherT
+
+trait PublicationValidationAlgebra[F[_]] {
+  /* Fails with a PublicationAlreadyExistsError */
+  // def doesNotExist(publication: Publication): EitherT[F, PublicationAlreadyExistsError, Unit]
+
+  /* Fails with a PetNotFoundError if the pet id does not exist or if it is none */
+  def exists(id: Option[Long]): EitherT[F, PublicationNotFoundError.type, Unit]
+}
+
+// ./Domain/publications/PublicationValidationInterpreter.scala
+import cats.Applicative
+import cats.data.EitherT
+import cats.implicits._
+
+class PublicationValidationInterpreter[F[_]: Applicative](repository: PublicationRepositoryAlgebra[F]) extends PublicationValidationAlgebra[F] {
+
+  def exists(id: Option[Long]): EitherT[F, PublicationNotFoundError.type, Unit] =
+    EitherT {
+      id match {
+        case Some(id) =>
+          repository.get(id).map {
+            case Some(_) => Right(())
+            case _ => Left(PublicationNotFoundError)
+          }
+        case _ =>
+          Either.left[PublicationNotFoundError.type, Unit](PublicationNotFoundError).pure[F]
+      }
+    }
+}
+
+object PublicationValidationInterpreter {
+  def apply[F[_]: Applicative](repository: PublicationRepositoryAlgebra[F]) =
+    new PublicationValidationInterpreter[F](repository)
+}
+
+// ./Domain/publications/PublicationService.scala
+
+import cats.Functor
+import cats.data._
+import cats.Monad
+import cats.syntax.all._
+
+/**
+  * The entry point to our domain, works with repositories and validations to implement behavior
+  * @param repository where we get our data
+  * @param validation something that provides validations to the service
+  * @tparam F - this is the container for the things we work with, could be scala.concurrent.Future, Option, anything
+  *           as long as it is a Monad
+  */
+class PublicationService[F[_]](
+  repository: PublicationRepositoryAlgebra[F],
+  validation: PublicationValidationAlgebra[F]
+) {
+
+  def get(id: Long)(implicit F: Functor[F]): EitherT[F, PublicationNotFoundError.type, Publication] =
+    EitherT.fromOptionF(repository.get(id), PublicationNotFoundError)
+
+  def findByAuthorId(authorId: Long): F[List[Publication]] =
+    repository.findByAuthorId(authorId)
+}
+
+object PublicationService {
+  def apply[F[_]](repository: PublicationRepositoryAlgebra[F], validation: PublicationValidationAlgebra[F]): PublicationService[F] =
+    new PublicationService[F](repository, validation)
+}
+
+
+// ./Infrastructure
+
+// ./Infrastruture/repository/doobie/DoobieAuthorRepositoryInterpreter.scala
+private object AuthorSQL {
+
+  def findOne(id: Long): Query0[Author] = sql"""
+    SELECT FIRST_NAME, LAST_NAME, EMAIL, PHONE, ID
+    FROM AUTHORS
+    WHERE ID = $id
+  """.query
+
+  def find(email: String): Query0[Author] = sql"""
+    SELECT FIRST_NAME, LAST_NAME, EMAIL, PHONE, ID
+    FROM AUTHORS
+    WHERE EMAIL = $email
+  """.query
+}
+
+// ./Infrastruture/repository/doobie/DoobiePublicationRepositoryInterpreter.scala
+private object PublicationSQL {
+  /* We require conversion for date time */
+  implicit val DateTimeMeta: Meta[Instant] =
+    Meta[java.sql.Timestamp].imap(_.toInstant)(java.sql.Timestamp.from)
+
+  def findOne(id: Long): Query0[Publication] = sql"""
+    SELECT ID, AUTHOR_ID, PUBLICATION_DATE, TITLE
+    FROM PUBLICATIONS
+    WHERE ID = $id
+  """.query[Publication]
+
+  def find(authorId: Long): Query0[Publication] = sql"""
+    SELECT ID, AUTHOR_ID, PUBLICATION_DATE, TITLE
+    FROM PUBLICATIONS
+    WHERE AUTHOR_ID = $authorId
+  """.query[Publication]
+  
+}
+
+
+
 // From: https://developer.ibm.com/tutorials/cl-model-first-microservices-scala-cats/
 
 case class Author(id: Long, name: String)
