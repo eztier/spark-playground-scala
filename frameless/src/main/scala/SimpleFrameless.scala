e00bf24a7cd557d81a97832b583037874f6c68c0
once-ler <htao@eztier.com>
2019-12-07 16:18:47 -0500

    Fix type and case class structure for frameless test module

--- frameless/src/main/scala/SimpleFrameless.scala
+++ frameless/src/main/scala/SimpleFrameless.scala
@@ -1,3 +1,9 @@
+package com.eztier.examples
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{SQLContext, Row, SparkSession, SaveMode}
+import cats.implicits._
+
 import frameless._
 import frameless.syntax._
 import frameless.ml._
@@ -5,41 +11,53 @@ import frameless.ml.feature._
 import frameless.ml.regression._
 import org.apache.spark.ml.linalg.Vector
 
-case class PatientVisitFromTo(patientPostalCode: String, visitFacility: String, patientAge: Int)
+case class PatientVisitFromTo(patientPostalCode: Int, visitFacility: Int, patientAge: Double)
 
-case class FacilityFeatures(patientPostalCode: String, visitFacility: String)
+case class FacilityFeatures(patientPostalCode: Int, visitFacility: Int)
 
 object SimpleFramelessApp {
   def main(args: Array[String]) {
 
+    val appID: String = new java.util.Date().toString + math.floor(math.random * 10E4).toLong.toString
+
+    val conf: SparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("Simple frameless app")
+      .set("spark.ui.enabled", "false")
+      .set("spark.app.id", appID)
+
+    implicit def session: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+    implicit def sc: SparkContext = session.sparkContext
+    
     val trainingData = TypedDataset.create(Seq(
-      PatientVisitFromTo("10001", "A", 43),
-      PatientVisitFromTo("10010", "B", 76)
+      PatientVisitFromTo(10001, 1, 43),
+      PatientVisitFromTo(10010, 2, 76)
     ))
 
     val assembler = TypedVectorAssembler[FacilityFeatures]
 
-    case class PatientVisitFromToWithFeatures(patientPostalCode: String, visitFacility: String, features: Vector)
+    case class PatientVisitFromToWithFeatures(patientPostalCode: Int, visitFacility: Int, patientAge: Double, features: Vector)
 
     val trainingDataWithFeatures = assembler.transform(trainingData).as[PatientVisitFromToWithFeatures]
 
     // Train the model.
-    case class RandomForestInputs(patientPostalCode: String, visitFacility: String, features: Vector)
+    case class RandomForestInputs(patientAge: Double, features: Vector)
     
     val rf = TypedRandomForestRegressor[RandomForestInputs]
     
     val model = rf.fit(trainingDataWithFeatures).run()
 
     // Prediction.
-    val testData = TypedDataset.create(Seq(PatientVisitFromTo("10018", "B", 0)))
+    val testData = TypedDataset.create(Seq(PatientVisitFromTo(10018, 2, 0)))
     
     val testDataWithFeatures = assembler.transform(testData).as[PatientVisitFromToWithFeatures]
     
     case class VisitFacilityPrediction(
-      postalCode: String, 
-      facility: String,
+      patientPostalCode: Int, 
+      visitFacility: Int,
+      patientAge: Double,
       features: Vector,
-      predictedAge: Int
+      predictedAge: Double
     )
     
     val predictions = model.transform(testDataWithFeatures).as[VisitFacilityPrediction]
