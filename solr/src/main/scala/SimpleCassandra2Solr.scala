54e7e513d8943ae8dbb11cc6cf98632dff526389
once-ler <htao@eztier.com>
2020-02-03 12:26:11 -0500

    Use udf to convert map to list b/c solr has no map type

--- solr/src/main/scala/SimpleCassandra2Solr.scala
+++ solr/src/main/scala/SimpleCassandra2Solr.scala
@@ -20,15 +20,77 @@ object SimpleCassandra2SolrApp {
   )
   
   def main(args: Array[String]) {
+    import com.datastax.spark.connector.cql._
+    
+    val spark: SparkSession = SparkSession.builder.appName("Simple cassandra 2 solr application").getOrCreate()
+    // Configure cassandra manually.
+    spark.setCassandraConf(cassandraOptions("cluster"), CassandraConnectorConf.ConnectionHostParam.option(cassandraOptions("host")) ++ CassandraConnectorConf.ConnectionPortParam.option(cassandraOptions("port").toInt))
+
+    import org.apache.spark.sql.types._
+    import org.apache.spark.sql.functions._
+    import org.apache.spark.sql.expressions.UserDefinedFunction
+    import spark.implicits._
+
+    val mapToListFunc: Map[String, String] => List[String] = m =>
+      m.toList.map(a => s"${a._1}:${a._2}")
+
+    val mapToListUdf: UserDefinedFunction = udf(mapToListFunc, DataTypes.createArrayType(DataTypes.StringType))
+    
+    val df = spark.sql(s"""
+      select * from dwh.ca_document_extracted limit 10
+    """)
+
+    val df2 = df.withColumn("metadatalist", mapToListUdf($"metadata"))
+      .select(
+        $"id",
+        $"domain",
+        $"root_type",
+        $"root_id",
+        $"root_owner",
+        $"root_associates",
+        $"root_company",
+        $"root_status",
+        $"root_display",
+        $"root_display_long",
+        $"doc_id",
+        $"doc_other_id",
+        $"doc_file_path",
+        $"doc_object_path",
+        $"doc_category",
+        $"doc_name",
+        $"doc_date_created",
+        $"doc_year_created",
+        $"content",
+        $"metadatalist".alias("metadata")
+      )
+
+      df2.write.format("solr").options(solrOptions).mode(org.apache.spark.sql.SaveMode.Overwrite).save
+  }  
+
+  def main2(args: Array[String]) {
+    import com.datastax.spark.connector._
+    import com.datastax.spark.connector.cql._
+    import org.apache.spark.SparkContext
+    
+    val spark: SparkSession = SparkSession.builder.appName("Simple cassandra 2 solr application").getOrCreate()
+    // Configure cassandra manually.
+    spark.setCassandraConf(cassandraOptions("cluster"), CassandraConnectorConf.ConnectionHostParam.option(cassandraOptions("host")) ++ CassandraConnectorConf.ConnectionPortParam.option(cassandraOptions("port").toInt))
+
+    val sc: SparkContext = spark.sparkContext
+    
+    val rdd = sc.cassandraTable("ca_document_extracted", "dwh")
+      .map { row =>
+        val n = row.getMap[String, String]("metadata").toList.map(a => s"${a._1}:${a._2}")
+      }
+  }
+
+  def main3(args: Array[String]) {
     
     val spark = SparkSession.builder.appName("Simple cassandra 2 solr application").getOrCreate()
     
-    // Configure cassandra manually.
-    // spark.setCassandraConf(cassandraOptions("cluster"), CassandraConnectorConf.ConnectionHostParam.option(cassandraOptions("host")) ++ CassandraConnectorConf.ConnectionPortParam.option(cassandraOptions("port").toInt))
-
     val df = spark
       .read
-      .cassandraFormat("document_extracted", "dwh")
+      .cassandraFormat("ca_document_extracted", "dwh")
       .options(cassandraOptions)
       .load()
 
